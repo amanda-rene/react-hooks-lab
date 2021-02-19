e28e89a7dbf4fe1cb57e3f4d1f8f713f1a80c370
Amanda Mattleman <amandamattleman@MSI>
2021-02-19 09:21:04 -0700

    fin

---
+++ app-3/src/Pokemon.js
@@ -0,0 +1,9 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+const Pokemon = (props) => {
+    const {name} = props.match.params
+    
+}
+
+export default Pokemon;
